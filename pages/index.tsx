666897e7960fa0dce0d2ce36e04685b0a94ec441
elyash14 <elyas_mosayebi@yahoo.com>
2021-01-23 00:26:39 +0330

    feat: initial next js app

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
