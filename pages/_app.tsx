7fdad4e5de9e3e8eafcc88b1b56116584aaee218
elyash14 <elyas_mosayebi@yahoo.com>
2021-01-25 01:14:34 +0330

    feat: initialize material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import "../src/styles/globals.css";
 import { Provider } from 'next-auth/client'
+import { useEffect } from "react";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import theme from "../src/configs/theme";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <Provider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
