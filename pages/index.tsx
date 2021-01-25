a8570b092df9cacafffe956a754051949a2ae2cd
elyash14 <elyas_mosayebi@yahoo.com>
2021-01-26 00:04:15 +0330

    feat: initialize material ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import Head from "next/head";
-import { signIn, signOut, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from "next-auth/client";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { useState } from "react";
 
 export default function Home() {
   const [session, loading] = useSession();
+  const [value, setValue] = useState(0);
 
   return (
     <div>
@@ -26,6 +32,17 @@ export default function Home() {
           </>
         )}
       </div>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </div>
   );
 }
--- src/configs/theme.ts
+++ src/configs/theme.ts
@@ -4,6 +4,16 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
   palette: {
     type: "dark",
+    primary:{
+        light: '#ed4b82',
+        main: '#e91e63',
+        dark: '#a31545'
+    },
+    secondary:{
+        light: '#ffb851',
+        main: '#ffa726',
+        dark: '#b2741a'
+    }
   },
 });
 
